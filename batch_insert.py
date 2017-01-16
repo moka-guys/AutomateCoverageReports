ac9cf26ee3a5b6c5db1a326f334d5845ea783e08
Aled Jones <aledjones@nhs.net>
2017-01-16 17:54:06 +0000

    change name of script to insert to db

--- batch_insert.py
+++ batch_insert.py
@@ -12,7 +12,5 @@ for i in os.listdir(directory):
     if i.startswith("imported"):
         pass
     else:
-        #print "python F:\Moka\Files\Software\depthofcoverage\AutomateCoverageReports\import_depth_of_coverage.py -d "+str(i.split("_")[2])
-        #print i
-        os.system("python F:\Moka\Files\Software\depthofcoverage\AutomateCoverageReports\import_depth_of_coverage.py -d " + str(i.split("_")[2]))
+        os.system("python F:\Moka\Files\Software\depthofcoverage\AutomateCoverageReports\insert_to_db.py -d " + str(i.split("_")[2]))
         os.rename(directory+"\\"+i, directory+"\imported\\"+i)
